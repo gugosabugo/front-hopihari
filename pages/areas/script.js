3db34fd97cc7a24e530926fa1aa3556afb2dc9b6
gugosabugo <liviasayurishirotargino@gmail.com>
2025-06-10 11:23:34 -0300

    Imagem brinquedo

--- pages/areas/script.js
+++ pages/areas/script.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', async function() {
         const grid = document.querySelector('.rides-grid');
         grid.innerHTML = grid.innerHTML + `
         <div class="ride-card">
-                <div class="ride-image" style="background-image: url('https://blog.hopihari.com.br/wp-content/uploads/2023/11/vamvolari-1024x683.jpg')"></div>
+                <div class="ride-image" style="background-image: url('../img/${brinquedo.image}')"></div>
                 <div class="ride-info">
                     <h3 class="ride-name">${brinquedo.name}</h3>
                     <div class="ride-time">${brinquedo.waiting_time}</div>
